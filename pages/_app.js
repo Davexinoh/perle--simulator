246702aae82ed94b80d5f5e72fc6223ddf8b2341
Davexinoh <Davexinoh@gmail.com>
2026-03-19 01:45:23 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
 import "../styles/globals.css";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    // Ping every 14 minutes to prevent Render from sleeping
+    const interval = setInterval(() => {
+      fetch("/api/ping").catch(() => {});
+    }, 14 * 60 * 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return <Component {...pageProps} />;
 }
